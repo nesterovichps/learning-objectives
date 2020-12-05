2a65904faa74cd3416c53c7d50a7e245d946ed72
petr <petr.n.92@list.ru>
2020-12-06 03:33:17 +0600

    end task 8 lesson 2 from GeekBrains

--- GeekBrains/lesson_2/8.py
+++ GeekBrains/lesson_2/8.py
@@ -3,9 +3,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-task = input("Введите график функции вида y = kx + b. Пример 2x-27 \n")
-k, b = map(int, task.split('x'))
-x = int(input("Введите х координату"))
+# task = input("Введите график функции вида y = kx + b. Пример 2x-27 \n")
+task = '-12x+11111140.2121' # Функция из задания
+k, b = map(float, task.split('x'))
+# x = int(input("Введите х координату"))
+x= 2.5 # Х из задания
 y = k * x + b
 print(f'Координате х = {x} соответствует У координата, равная {y}')
 fig = plt.subplots()
