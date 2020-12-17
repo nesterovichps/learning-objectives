3e8dd6f8a014a8d344ece17437eb606b47d7941a
petr <petr.n.92@list.ru>
2020-12-17 23:46:28 +0600

    good get response all page

--- parser/mlsn.py
+++ parser/mlsn.py
@@ -1,21 +1,31 @@
-import requests
+import requests ,time
 from bs4 import BeautifulSoup as bs
 def all_list_page( http_link_universal):
-    get_page(http_link_universal,1)
-
+    number_page=1
+    page=True
+    while page:#
+        time.sleep(100)
+        page=get_page(http_link_universal,number_page)
+        number_page+=1
+        pars_page(page)
 def get_page(http_link_universal,number_page):
-    r=requests.get(http_link_universal.format(number_page))
-
+    page=requests.get(http_link_universal.format(number_page))
+    if 'По вашему запросу ничего не найдено.' in page.text:
+        return False
     print(http_link_universal.format(number_page))
-    print(r.text)
+    print(number_page)
+    # print(r.text)
+    return page
 def pars_page():
     pass
+
 def save_result():
     pass
-def main( http_link_universal):
+
+def main():
+    http_link_universal='https://omsk.mlsn.ru/pokupka-nedvizhimost/?district=52401372%2C52401376%2C52401380%2C52401382%2Ccenter&objectTypeId=1%2C2%2C3&onlyOwners=1&page={}&priceMax=70000&priceUnitId=2'
     all_list_page( http_link_universal)
 
 if __name__ == '__main__':
-    http_link_universal='https://omsk.mlsn.ru/pokupka-nedvizhimost/?district=52401372%2C52401376%2C52401380%2C52401382%2Ccenter&objectTypeId=1%2C2%2C3&onlyOwners=1&page={}&priceMax=70000&priceUnitId=2'
-    main( http_link_universal)
+    main()
 
