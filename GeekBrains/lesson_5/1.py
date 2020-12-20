872328fc6fbb1d4c6c18460da5fcf2fbca7ddc65
petr <petr.n.92@list.ru>
2020-12-20 22:22:09 +0600

    lesson 5 task 2 end GeekBrain

--- GeekBrains/lesson_5/1.py
+++ GeekBrains/lesson_5/1.py
@@ -3,11 +3,19 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 import os
+
+
 def add_dir():
-    for i in range(1,10):
-        os.mkdir(f'dir_{i}')
+    try:
+        for i in range(1,10):
+            os.mkdir(f'dir_{i}')
+    except:
+        pass
 def del_dir():
-    for i in range(1,10):
-        os.rmdir(f'dir_{i}')
-# add_dir()
+    try:
+        for i in range(1,10):
+            os.rmdir(f'dir_{i}')
+    except:
+        pass
+add_dir()
 # del_dir()
---
+++ GeekBrains/lesson_5/2.py
@@ -0,0 +1,10 @@
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+import os
+
+
+def show_dir_this():
+    print(os.listdir())
+
+
+show_dir_this()
