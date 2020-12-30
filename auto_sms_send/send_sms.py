3a582fb4e1b1568c62e74df0b731ea6710cd75bf
petr <petr.n.92@list.ru>
2020-12-31 02:16:10 +0600

    sms sender

--- auto_sms_send/send_sms.py
+++ auto_sms_send/send_sms.py
@@ -1,17 +1,27 @@
 import requests
 import time
 def send_sms(phone,params):
-    params['phone']=phone
-    print(phone)
-    print(requests.get(url,params=params).text)
+    with open('used_number.txt','r+',encoding='utf-8') as number_file:
+        if phone[1:] not in number_file.read():
+            print('good')
+            params['phone'] = phone
+            print(phone)
+            print(requests.get(url,params=params).text)
+            number_file.writelines(phone[1:] + '\n')
+        else:
+            print('bad')
+            print(phone)
 url='https://semysms.net/api/3/sms.php'
 TOKEN='953cc1ada634323447cecc90f10eba04'
 device='active'
-# phone='+79503333343'
 
-msg='test'
+
+msg='''Продам Вашу квартиру Быстро, Качественно, Без нервов!
+Петр. Специалист по недвижимости и ипотеки Мегаполис. 
+тел 89503333343 
+https://onlinevizitka.com/petr_megapolis/
+Звоните! Сейчас самое время продавать.'''
 params={'token':TOKEN,'device':device,'msg':msg}
-# print(requests.get(url,params=params).text)
 with open('number.txt','r',encoding='utf-8') as file:
     for line in file.readlines():
         phone='+7'+str(line.strip())[-10:]
