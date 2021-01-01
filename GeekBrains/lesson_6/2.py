13d788ca110fdf10386204bf94531d5f0ee08518
petr <petr.n.92@list.ru>
2021-01-02 01:36:44 +0600

    task 2 lesson 6 GeekBrains

--- GeekBrains/lesson_6/2.py
+++ GeekBrains/lesson_6/2.py
@@ -1,9 +1,6 @@
-# Задача - 1
-# Опишите несколько классов TownCar, SportCar, WorkCar, PoliceCar
-# У каждого класса должны быть следующие аттрибуты:
-# speed, color, name, is_police - Булево значение.
-# А так же несколько методов: go, stop, turn(direction) - которые должны сообщать,
-#  о том что машина поехала, остановилась, повернула(куда)
+# Задача - 2
+# Посмотрите на задачу-1 подумайте как выделить общие признаки классов
+# в родительский и остальные просто наследовать от него.
 
 
 class Car():
@@ -51,26 +48,13 @@ class PoliceCar(Car):
 
 
 town_car = TownCar(90, 'white', 'bmw')
-town_car.show_me()
-town_car.go()
-town_car.turn('left')
-town_car.turn('right')
-town_car.stop()
 sport_car = SportCar(200, 'black', 'mersedes')
-sport_car.show_me()
-sport_car.go()
-sport_car.turn('left')
-sport_car.turn('right')
-sport_car.stop()
 work_car = WorkCar(60, 'yellow', 'maz')
-work_car.show_me()
-work_car.go()
-work_car.turn('left')
-work_car.turn('right')
-work_car.stop()
 police_car = PoliceCar(120, 'blue', 'shkoda')
-police_car.show_me()
-police_car.go()
-police_car.turn('left')
-police_car.turn('right')
-police_car.stop()
+
+for car in [town_car, sport_car, work_car, police_car]:
+    car.show_me()
+    car.go()
+    car.turn('left')
+    car.turn('right')
+    car.stop()
